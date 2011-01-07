c96b4aa66b4ecd6a9ca1856f0bd2f44bca29573d
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-07 15:52:29 +0000

    3.1+ compatibility update
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/users-widget@2056 f53fe46e-8c02-459b-9fdd-93e75771b278

--- widget-users.php
+++ widget-users.php
@@ -1,10 +1,13 @@
 <?php
 /*
 Plugin Name: Users Widget
-Description:
-Author: Andrew Billits (Incsub)
-Version: 1.0.0
-Author URI:
+Plugin URI: http://premium.wpmudev.org/project/users-widget
+Description: Show a nice list of random users from your site, with avatars, wherever you want with this handy widget
+Author: S H Mohanjith (Incsub), Andrew Billits (Incsub)
+Version: 1.0.1
+Author URI: http://premium.wpmudev.org
+WDP ID: 63
+Network: true
 */
 
 /* 
@@ -24,6 +27,15 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+add_action('init', 'widget_users_init');
+
+function widget_users_init() {
+	if ( !is_multisite() )
+		exit( 'The Widget Blogs plugin is only compatible with WordPress Multisite.' );
+		
+	load_plugin_textdomain('widget_users', false, dirname(plugin_basename(__FILE__)).'/languages');
+}
+
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -35,7 +47,7 @@ $users_widget_main_blog_only = 'yes'; //Either 'yes' or 'no'
 //------------------------------------------------------------------------//
 //---Functions------------------------------------------------------------//
 //------------------------------------------------------------------------//
-function widget_users_init() {
+function widget_users_widget_init() {
 	global $wpdb, $users_widget_main_blog_only;
 		
 	// Check for the required API functions
@@ -61,18 +73,18 @@ function widget_users_init() {
 	?>
 				<div style="text-align:left">
                 
-				<label for="users-title" style="line-height:35px;display:block;"><?php _e('Title', 'widgets'); ?>:<br />
+				<label for="users-title" style="line-height:35px;display:block;"><?php _e('Title', 'widgets', 'widget_users'); ?>:<br />
                 <input class="widefat" id="users-title" name="users-title" value="<?php echo $options['users-title']; ?>" type="text" style="width:95%;">
                 </select>
                 </label>
-				<label for="users-display" style="line-height:35px;display:block;"><?php _e('Display', 'widgets'); ?>:
+				<label for="users-display" style="line-height:35px;display:block;"><?php _e('Display', 'widgets', 'widget_users'); ?>:
                 <select name="users-display" id="users-display" style="width:95%;">
-                <option value="avatar_display_name" <?php if ($options['users-display'] == 'avatar_display_name'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar + Display Name'); ?></option>
-                <option value="avatar" <?php if ($options['users-display'] == 'avatar'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar Only'); ?></option>
-                <option value="display_name" <?php if ($options['users-display'] == 'display_name'){ echo 'selected="selected"'; } ?> ><?php _e('Display Name Only'); ?></option>
+                <option value="avatar_display_name" <?php if ($options['users-display'] == 'avatar_display_name'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar + Display Name', 'widget_users'); ?></option>
+                <option value="avatar" <?php if ($options['users-display'] == 'avatar'){ echo 'selected="selected"'; } ?> ><?php _e('Avatar Only', 'widget_users'); ?></option>
+                <option value="display_name" <?php if ($options['users-display'] == 'display_name'){ echo 'selected="selected"'; } ?> ><?php _e('Display Name Only', 'widget_users'); ?></option>
                 </select>
                 </label>
-				<label for="users-display-name-characters" style="line-height:35px;display:block;"><?php _e('Display Name Characters', 'widgets'); ?>:<br />
+				<label for="users-display-name-characters" style="line-height:35px;display:block;"><?php _e('Display Name Characters', 'widgets', 'widget_users'); ?>:<br />
                 <select name="users-display-name-characters" id="users-display-name-characters" style="width:95%;">
                 <?php
 					if ( empty($options['users-display-name-characters']) ) {
@@ -87,13 +99,13 @@ function widget_users_init() {
                 ?>
                 </select>
                 </label>
-				<label for="users-order" style="line-height:35px;display:block;"><?php _e('Order', 'widgets'); ?>:
+				<label for="users-order" style="line-height:35px;display:block;"><?php _e('Order', 'widgets', 'widget_users'); ?>:
                 <select name="users-order" id="users-order" style="width:95%;">
-                <option value="most_recent" <?php if ($options['users-order'] == 'most_recent'){ echo 'selected="selected"'; } ?> ><?php _e('Most Recent'); ?></option>
-                <option value="random" <?php if ($options['users-order'] == 'random'){ echo 'selected="selected"'; } ?> ><?php _e('Random'); ?></option>
+                <option value="most_recent" <?php if ($options['users-order'] == 'most_recent'){ echo 'selected="selected"'; } ?> ><?php _e('Most Recent', 'widget_users'); ?></option>
+                <option value="random" <?php if ($options['users-order'] == 'random'){ echo 'selected="selected"'; } ?> ><?php _e('Random', 'widget_users'); ?></option>
                 </select>
                 </label>
-				<label for="users-number" style="line-height:35px;display:block;"><?php _e('Number', 'widgets'); ?>:<br />
+				<label for="users-number" style="line-height:35px;display:block;"><?php _e('Number', 'widgets', 'widget_users'); ?>:<br />
                 <select name="users-number" id="users-number" style="width:95%;">
                 <?php
 					if ( empty($options['users-number']) ) {
@@ -108,13 +120,13 @@ function widget_users_init() {
                 ?>
                 </select>
                 </label>
-				<label for="users-avatar-size" style="line-height:35px;display:block;"><?php _e('Avatar Size', 'widgets'); ?>:<br />
+				<label for="users-avatar-size" style="line-height:35px;display:block;"><?php _e('Avatar Size', 'widgets', 'widget_users'); ?>:<br />
                 <select name="users-avatar-size" id="users-avatar-size" style="width:95%;">
-                <option value="16" <?php if ($options['users-avatar-size'] == '16'){ echo 'selected="selected"'; } ?> ><?php _e('16px'); ?></option>
-                <option value="32" <?php if ($options['users-avatar-size'] == '32'){ echo 'selected="selected"'; } ?> ><?php _e('32px'); ?></option>
-                <option value="48" <?php if ($options['users-avatar-size'] == '48'){ echo 'selected="selected"'; } ?> ><?php _e('48px'); ?></option>
-                <option value="96" <?php if ($options['users-avatar-size'] == '96'){ echo 'selected="selected"'; } ?> ><?php _e('96px'); ?></option>
-                <option value="128" <?php if ($options['users-avatar-size'] == '128'){ echo 'selected="selected"'; } ?> ><?php _e('128px'); ?></option>
+                <option value="16" <?php if ($options['users-avatar-size'] == '16'){ echo 'selected="selected"'; } ?> ><?php _e('16px', 'widget_users'); ?></option>
+                <option value="32" <?php if ($options['users-avatar-size'] == '32'){ echo 'selected="selected"'; } ?> ><?php _e('32px', 'widget_users'); ?></option>
+                <option value="48" <?php if ($options['users-avatar-size'] == '48'){ echo 'selected="selected"'; } ?> ><?php _e('48px', 'widget_users'); ?></option>
+                <option value="96" <?php if ($options['users-avatar-size'] == '96'){ echo 'selected="selected"'; } ?> ><?php _e('96px', 'widget_users'); ?></option>
+                <option value="128" <?php if ($options['users-avatar-size'] == '128'){ echo 'selected="selected"'; } ?> ><?php _e('128px', 'widget_users'); ?></option>
                 </select>
                 </label>
 				<input type="hidden" name="users-submit" id="users-submit" value="1" />
@@ -134,7 +146,7 @@ function widget_users_init() {
 
 		?>
 		<?php echo $before_widget; ?>
-			<?php echo $before_title . __($options['users-title']) . $after_title; ?>
+			<?php echo $before_title . __($options['users-title'], 'widget_users') . $after_title; ?>
             <br />
             <?php
 
@@ -181,15 +193,23 @@ function widget_users_init() {
 	// Tell Dynamic Sidebar about our new widget and its control
 	if ( $users_widget_main_blog_only == 'yes' ) {
 		if ( $wpdb->blogid == 1 ) {
-			register_sidebar_widget(array(__('Users'), 'widgets'), 'widget_users');
-			register_widget_control(array(__('Users'), 'widgets'), 'widget_users_control');
+			register_sidebar_widget(array(__('Users', 'widget_users'), 'widgets'), 'widget_users');
+			register_widget_control(array(__('Users', 'widget_users'), 'widgets'), 'widget_users_control');
 		}
 	} else {
-		register_sidebar_widget(array(__('Users'), 'widgets'), 'widget_users');
-		register_widget_control(array(__('Users'), 'widgets'), 'widget_users_control');
+		register_sidebar_widget(array(__('Users', 'widget_users'), 'widgets'), 'widget_users');
+		register_widget_control(array(__('Users', 'widget_users'), 'widgets'), 'widget_users_control');
 	}
 }
 
-add_action('widgets_init', 'widget_users_init');
+add_action('widgets_init', 'widget_users_widget_init');
 
-?>
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
+}
